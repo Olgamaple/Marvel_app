856c188d34151273fcf84f29e1949ea3493981c4
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2022-04-27 23:26:13 +0300

    wrote networking and data transformation functionality

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=5b25ac1acda703411c8d21f5cdae9352';
+  
   getResource = async (url) => {
     let res = await fetch(url);
 
@@ -12,13 +13,26 @@ class MarvelService {
     return await res.json();
   }
 
-  getAllCharacters = () => {
-    return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharater);
   }
 
-  getCharacters = (id) => {
-    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-}
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharater(res.data.results[0]);
+  }
+
+  _transformCharater = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
+  }
+
 }
 
 export default MarvelService;
