cbb014e428a8cd52afebb3dd14cd26fd5ad7fb13
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2022-08-28 01:28:54 +0300

    used the componentDidUpdate lifecycle

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import Spinner from '../spinner/Spinner';
 import MarvelService from '../../services/MarvelService';
-import ErrorMessage from '../errorMessage/errorMessage';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
@@ -26,7 +26,7 @@ class RandomChar extends Component {
 
     onCharLoaded = (char) => {
         this.setState({
-            char : char,
+            char,
             loading: false
         })
     }
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,37 +1,39 @@
 
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+
   _apiKey = 'apikey=5b25ac1acda703411c8d21f5cdae9352';
-  
+
   getResource = async (url) => {
-    let res = await fetch(url);
-
-    if(!res.ok) {
-      throw new Error(`Coould not fetch ${url}, status: ${res.status}`);
-    }
-
-    return await res.json();
+      let res = await fetch(url);
+  
+      if (!res.ok) {
+          throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+      }
+  
+      return await res.json();
   }
 
   getAllCharacters = async () => {
-    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-    return res.data.results.map(this._transformCharater);
+      const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+      return res.data.results.map(this._transformCharacter);
   }
 
   getCharacter = async (id) => {
-    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-    return this._transformCharater(res.data.results[0]);
+      const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+      return this._transformCharacter(res.data.results[0]);
   }
 
-  _transformCharater = (char) => {
-    return {
-      id: char.id,
-      name: char.name,
-      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
-      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
-      homepage: char.urls[0].url,
-      wiki: char.urls[1].url
-    }
+  _transformCharacter = (char) => {
+      return {
+          id: char.id,
+          name: char.name,
+          description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+          thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+          homepage: char.urls[0].url,
+          wiki: char.urls[1].url,
+          comics: char.comics.items
+      }
   }
 }
 
