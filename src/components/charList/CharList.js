5f5555bfccce6601aee2df7edacf9df8823a3afa
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2022-09-12 20:56:58 +0300

    added pagination functionality to the data pagination

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -9,23 +9,43 @@ class CharList extends Component {
     state = {
         charList: [],
         loading: true,
-        error: false
+        error: false,
+        newItemLoading: false,
+        offset: 1551,
+        charEnded: false
     }
 
     marvelService = new MarvelService();
 
     componentDidMount() {
+        this.onRequest();
+    }
 
-        this.marvelService.getAllCharacters()
+    onRequest = (offset) => {
+        this.onCharListLoading();
+        this.marvelService.getAllCharacters(offset)
         .then(this.onCharListLoaded)
         .catch(this.onError)
     }
 
-    onCharListLoaded = (charList) => {
+    onCharListLoading = () => {
         this.setState({
-            charList,
-            loading: false
+            newItemLoading: true
         })
+    }
+
+    onCharListLoaded = (newCharList) => {
+        let ended = false;
+        if (newCharList.length < 9) {
+            ended = true;
+        }
+        this.setState(({offset, charList}) => ({
+                charList: [...charList, ...newCharList],
+                loading: false,
+                newItemLoading: false,
+                offset: offset + 9,
+                charEnded: ended
+            }))
     }
 
     onError = () => {
@@ -62,8 +82,8 @@ class CharList extends Component {
 
     render() {
 
-        const {charList, loading, error} = this.state;
-        
+        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
+
         const items = this.renderItems(charList);
 
         const errorMessage = error ? <ErrorMessage/> : null;
@@ -75,7 +95,11 @@ class CharList extends Component {
                 {errorMessage}
                 {spinner}
                 {content}
-                <button className="button button__main button__long">
+                <button
+                 className="button button__main button__long"
+                 disabled={newItemLoading}
+                 onClick={() => this.onRequest(offset)}
+                 style={{'display': charEnded ? 'none' : 'block'}}>
                     <div className="inner">load more</div>
                 </button>
             </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,6 +3,7 @@ class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
 
   _apiKey = 'apikey=5b25ac1acda703411c8d21f5cdae9352';
+  _baseOffset = 210;
 
   getResource = async (url) => {
       let res = await fetch(url);
@@ -14,8 +15,8 @@ class MarvelService {
       return await res.json();
   }
 
-  getAllCharacters = async () => {
-      const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  getAllCharacters = async (offset = this._offset) => {
+      const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
       return res.data.results.map(this._transformCharacter);
   }
 
