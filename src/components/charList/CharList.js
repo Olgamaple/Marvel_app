378fba5eaee0aaaf5639247450d36f4698c3221f
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2022-09-30 19:04:35 +0300

    heroku

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -13,7 +13,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 1551,
+        offset: 210,
         charEnded: false
     }
 
