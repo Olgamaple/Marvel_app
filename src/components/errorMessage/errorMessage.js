50a8cbaf194cb3b91d6a46c4f1f8e46cdd704e8c
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2022-10-03 13:01:02 +0300

    correction error

--- src/components/errorMessage/errorMessage.js
+++ src/components/errorMessage/errorMessage.js
@@ -5,4 +5,5 @@ const ErrorMessage = () => {
     <img style={{display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto'}} src={img} alt ='Error'/>
   )
 }
+
 export default ErrorMessage;
