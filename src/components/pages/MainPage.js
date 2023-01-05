503a6276f854f02f42a059ad01d4a76f3ce4bbf7
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2023-01-05 17:37:10 +0300

    incorrect route and no error found

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,35 +1,42 @@
-import {useState} from "react";
+import { useState } from "react";
+
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import CharSearchForm from '../charSearchForm/CharSearchForm';
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+
 import decoration from '../../resources/img/vision.png';
 
 const MainPage = () => {
 
-  const [selectedChar, setChar] = useState(null);
+    const [selectedChar, setChar] = useState(null);
 
-  const onCharSelected = (id) => {
-      setChar(id);
-      }
+    const onCharSelected = (id) => {
+        setChar(id);
+    }
 
-
-  return (
-    <>
-      <ErrorBoundary>
-          <RandomChar/>
-      </ErrorBoundary>
-        <div className="char__content">
+    return (
+        <>
             <ErrorBoundary>
-                <CharList onCharSelected={onCharSelected}/>
+                <RandomChar/>
             </ErrorBoundary>
-            <ErrorBoundary>
-                <CharInfo charId={selectedChar}/>
-            </ErrorBoundary>
-        </div>
+            <div className="char__content">
+                <ErrorBoundary>
+                    <CharList onCharSelected={onCharSelected}/>
+                </ErrorBoundary>
+                <div>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharSearchForm/>
+                    </ErrorBoundary>
+                </div>
+            </div>
             <img className="bg-decoration" src={decoration} alt="vision"/>
-    </>
-  )
+        </>
+    )
 }
 
 export default MainPage;
